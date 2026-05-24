656cbd80af293328e6c08b9e7754fcb17df65621
M.S. Ben <142302587+msbCyricTohoku@users.noreply.github.com>
2026-05-24 22:11:14 +0000

    fixes

--- artixinstall.sh
+++ artixinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 echo "=========================================="
 echo " Welcome to artixinstall (OpenRC Edition) "
@@ -13,69 +14,62 @@ read -p "Enter username for the new user: " USERNAME
 read -s -p "Enter password for root and $USERNAME: " PASSWORD
 echo -e "\n"
 
-echo "WARNING: This will completely wipe $DISK."
 read -p "Are you sure you want to continue? (y/N): " CONFIRM
 if [[ "$CONFIRM" != "y" && "$CONFIRM" != "Y" ]]; then
-    echo "Installation aborted."
     exit 1
 fi
 
-echo "=> Partitioning $DISK (UEFI)..."
 parted -s "$DISK" mklabel gpt
-parted -s "$DISK" mkpart ESP fat32 1MiB 513MiB
-parted -s "$DISK" set 1 esp on
-parted -s "$DISK" mkpart primary ext4 513MiB 100%
+parted -s "$DISK" mkpart primary 1MiB 3MiB
+parted -s "$DISK" set 1 bios_grub on
+parted -s "$DISK" mkpart ESP fat32 3MiB 515MiB
+parted -s "$DISK" set 2 esp on
+parted -s "$DISK" mkpart primary ext4 515MiB 100%
 
 if [[ "$DISK" == *"nvme"* ]]; then
-    PART_BOOT="${DISK}p1"
-    PART_ROOT="${DISK}p2"
+    PART_BOOT="${DISK}p2"
+    PART_ROOT="${DISK}p3"
 else
-    PART_BOOT="${DISK}1"
-    PART_ROOT="${DISK}2"
+    PART_BOOT="${DISK}2"
+    PART_ROOT="${DISK}3"
 fi
 
-echo "=> Formatting partitions..."
 mkfs.fat -F 32 "$PART_BOOT"
 mkfs.ext4 -F "$PART_ROOT"
 
-echo "=> Mounting partitions..."
 mount "$PART_ROOT" /mnt
 mkdir -p /mnt/boot/efi
 mount "$PART_BOOT" /mnt/boot/efi
 
-echo "=> Installing base system and OpenRC packages..."
 basestrap /mnt base base-devel openrc elogind-openrc linux linux-firmware nano networkmanager-openrc grub efibootmgr
 
-echo "=> Generating fstab..."
 fstabgen -U /mnt >> /mnt/etc/fstab
-
-echo "=> Configuring the system..."
 
 cat <<EOF > /mnt/chroot-install.sh
 #!/bin/bash
+set -e
 
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 hwclock --systohc
 
-# Locale
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# Hostname and Network (OpenRC way)
 echo "$HOSTNAME" > /etc/hostname
-# Enable NetworkManager in OpenRC
 rc-update add NetworkManager default
 
-# Users & Passwords
-echo "root:$PASSWORD" | chpasswd
+echo "root:${PASSWORD}" | chpasswd
 useradd -m -G wheel -s /bin/bash "$USERNAME"
-echo "$USERNAME:$PASSWORD" | chpasswd
-# Allow 'wheel' group to use sudo
+echo "${USERNAME}:${PASSWORD}" | chpasswd
+
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-# Bootloader (GRUB for UEFI)
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+if [ -d /sys/firmware/efi ]; then
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+else
+    grub-install --target=i386-pc "$DISK"
+fi
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
